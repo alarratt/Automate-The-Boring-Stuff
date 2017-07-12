7017579bcbb59dc11e51827b9ffa01f78b0ac070
Anthony <anthonylarratt@gmail.com>
2017-07-12 07:57:52 -0400

    Startin Automate

---
+++ Your_First_Program.py
@@ -0,0 +1,7 @@
+#This Program says hello and askes for my name. 
+
+print('Hello World')
+print('What is your name?')
+myName = input()
+print ('It is good to meet you, ' + myName)
+your name is (len(myName))
