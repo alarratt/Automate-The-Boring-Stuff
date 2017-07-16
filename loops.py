8d7f32b0ba9150789cd027146448e95dee266c70
Anthony <anthonylarratt@gmail.com>
2017-07-16 08:54:25 -0400

    July15

---
+++ loops.py
@@ -0,0 +1,10 @@
+#Ths coed corresponds with questions 13 and 14 for chapter 2 of Automate the Boring Stuff
+
+#For loop
+for i in range(1,11):
+    print (i) 
+
+i=1
+while i<=10:
+    print (i)
+    i=i+1
